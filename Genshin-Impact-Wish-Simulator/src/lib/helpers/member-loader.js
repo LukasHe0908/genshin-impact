0ad358a9ce86c87788d55f4c678ce7286441f3a6
Lukas <52617815+LukasHe0908@users.noreply.github.com>
2025-04-09 23:48:57 +0800

    test member loader

--- Genshin-Impact-Wish-Simulator/src/lib/helpers/member-loader.js
+++ Genshin-Impact-Wish-Simulator/src/lib/helpers/member-loader.js
@@ -4,6 +4,16 @@ import { localMemberList } from "./dataAPI/api-localstore";
 
 export let memberDB = [];
 
+const fooCharSet = new Set([
+    "诃", "呵", "抲", "峆", "哬", "喝", "訶", "嗬",
+    "禾", "纥", "合", "何", "劾", "咊", "和", "姀", "河", "郃", "紇", "曷", "柇", "狢", "盇", "阂", "饸",
+    "釛", "核", "盉", "盍", "荷", "俰", "趷", "啝", "涸", "盒", "秴", "菏", "萂", "蚵", "龁", "惒", "訸",
+    "颌", "嵑", "廅", "蓋", "滆", "楁", "毼", "詥", "貈", "貉", "鉌", "阖", "澕", "蒚", "閤", "熆", "閡",
+    "鹖", "褐", "頜", "蝎", "篕", "翮", "螛", "魺", "餲", "礉", "闔", "鞨", "齕", "覈", "鶡", "龢",
+    "吓", "咊", "佫", "贺", "袔", "隺", "寉", "賀", "猲", "嗃", "煂", "碋", "暍", "熇", "赫", "鹤",
+    "翯", "壑", "癋", "嚇", "爀", "蠚", "皬", "鶴", "靎", "鸖", "靍", "靏"
+]);
+
 memberList.subscribe((v) => {
 	memberDB = v;
 });
@@ -23,6 +33,11 @@ export const loadMembers = (f) => {
         lines.forEach((lineContent, idx) => {
             if (lineContent.trim() !== '') {
                 let arr = lineContent.split(' ');
+		let chineseChar = arr.length == 2? arr[1].trim() : lineContent.trim();
+		let firstChar = chineseChar[0];
+                if (fooCharSet.has(firstChar)) {
+                    return; // 跳过
+		}
                 data.push({
                     "name": `n-${idx}`,
                     "chineseChar": arr.length == 2? arr[1].trim() : lineContent.trim(),
@@ -38,9 +53,6 @@ export const loadMembers = (f) => {
     };
 
     reader.readAsText(f, 'UTF-8');
-    while (!reader.readyState === FileReader.DONE) {
-        // 空循环等待
-    }
 
     // console.log(content);
     // return content;
