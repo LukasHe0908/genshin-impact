b03e644905f7d2f6f428025b9c378f0cf38c96fb
Lukas <52617815+LukasHe0908@users.noreply.github.com>
2025-04-10 14:10:12 +0800

    Update svelte.config.js

--- Genshin-Impact-Wish-Simulator/svelte.config.js
+++ Genshin-Impact-Wish-Simulator/svelte.config.js
@@ -1,20 +1,17 @@
-import adapter from '@sveltejs/adapter-static';
+import path from 'path';
+import adapter from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined, // 或 '200.html'，如果你需要 fallback 支持
-			precompress: false,
-			strict: true
-		})
+		appDir: 'internal',
+		adapter: adapter(),
+		alias: {
+			$post: path.resolve('./src/post')
+		}
 	},
-	preprocess: preprocess({
-		postcss: true // 如果你用到 PostCSS，可根据需要配置其他选项
-	})
+	preprocess: preprocess({ postcss: true })
 };
 
 export default config;
